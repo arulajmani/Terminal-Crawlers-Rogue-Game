3f6c76972a51ba0f2b833d579555edd893831a7a
adityamaru27 <a2maru@uwaterloo.ca>
2016-07-15 19:18:16 -0400

    Dragon Observer Pattern

---
+++ publisherdragonhoard.cc
@@ -0,0 +1,28 @@
+#include publisherdragonhoard.h
+#include subscriberdragon.h
+
+PublisherDragonHoard::PublisherDragonHoard():Gold(value), dragon{nullptr}{}
+
+PublisherDragonHoard::~PublisherDragonHoard(){}
+
+void PublisherDragonHoard::attach(SubscriberDragon* subDragon){
+	dragon = subDragon;
+}
+
+void PublisherDragonHoard::detatchObservers(){
+	dragon = nullptr;
+}
+
+bool PublisherDragonHoard::canPickup(){
+	if(dragon == nullptr){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void PublisherDragonHoard::notifyObservers(){
+	dragon->notify();
+}
+
--- publisherdragonhoard.h
+++ publisherdragonhoard.h
@@ -5,14 +5,15 @@
 class SubscriberDragon;
 
 class PublisherDragonHoard : public Gold  {
+	const value = 8;
 	SubscriberDragon *dragon; // There always has to be one dragon, so not a vector
 public: 
 	PublisherDragonHoard();
 	virtual ~PublisherDragonHoard()=0;
 
-	void attach(SubscriberDragon subDragon);
+	void attach(SubscriberDragon *subDragon);
 	void notifyObservers();
-	void detachObservers(); // Once the dragon is destroyed, must detach to make the pointer null.
+	void detatchObservers(); // Once the dragon is destroyed, must detach to make the pointer null.
 	bool canPickup(); // Check if the dragon pointer is null, only then can be picked up
 };
 
