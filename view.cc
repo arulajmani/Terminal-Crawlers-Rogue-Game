866f17f78fa427970a6ba3c91a665c1e37f5c798
Arul Ajmani <arulajmani@v1020-wn-119-52.campus-dynamic.uwaterloo.ca>
2016-07-21 18:42:54 -0400

    View class

---
+++ view.cc
@@ -0,0 +1,21 @@
+#include "view.h"
+
+using namespace std;
+
+View::View() {}
+View::~View() {}
+
+void View::updateAt(std::pair<int, int> coords, char updateTo) {
+	displayBoard[get<0>(coords)][get<1>coords] = updateTo;
+}
+	void View::setBoard(char theBoard[numRows][numCols]) {
+		for(int i = 0; i < numRows; ++i) {
+			for(int j = 0; j < numCols; ++j) {
+				displayBoard[i][j] = theBoard[i][j];
+			}
+		}
+	}
+	void addMessage(std::string message) {
+		dispalyMessage << message;
+
+	}
---
+++ view.h
@@ -0,0 +1,19 @@
+#ifndef _VIEW_H_
+#define _VIEW_H_
+#include <string>
+#include <sstream>
+#include <utility>
+
+const int numRows = 25;
+const int numCols = 79;
+class View {
+	char displayBoard [numRows][numCols];
+	std::ostringstream displayMessage; 
+public:
+	View();
+	~View();
+
+	void updateAt(std::pair<int, int> coords, char updateTo);
+	void setBoard(char theBoard[numRows][numCols]);
+	void addMessage(std::string message);
+}
