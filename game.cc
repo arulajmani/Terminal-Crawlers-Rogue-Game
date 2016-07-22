daaf0339aa6fca2a28eef011274ddddc92302eb0
adityamaru27 <a2maru@uwaterloo.ca>
2016-07-22 18:14:12 -0400

    Game added

---
+++ game.cc
@@ -0,0 +1,38 @@
+#include "game.h"
+
+Game::Game(): floorNum{0} myPlayer{nullptr}, floor{nullptr} {
+	view = make_shared<View>();
+} 
+
+Game::~Game() {}
+
+void Game::Display() {
+	view->display();
+}
+
+void Game::init(bool filePresent,char *floorPlan) {
+	floorNum += 1;
+	if (floorNum == 6) {
+		// Print victory stuff. 
+	}
+	floor = make_shared<Floor>(floorNum, myPlayer, filePresent, floorPlan, view);
+}
+
+void Game::createPlayer(std::string race) {
+	myPlayer = factory.createPlayer(race);
+}
+
+void Game::movePlayer(std::string direction) {
+	floor->movePlayer(direction);
+}
+
+void Game::usePotion(std::string direction) {
+	floor->pickPotion(direction);
+}
+void Game::attackEnemy(std::string direction) {
+	floor->playerAttack(direction);
+}
+
+bool Game::isDead() {
+	return !(myPlayer->getHP());
+}
